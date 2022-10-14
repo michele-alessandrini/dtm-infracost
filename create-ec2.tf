03ac4173cee594bc9665432ef402433c26893153
Michele Alessandrini <michele.alessandrini@gmail.com>
2022-10-14 22:04:30 +0200

    8xlarge

--- create-ec2.tf
+++ create-ec2.tf
@@ -5,7 +5,7 @@ module "ec2_instance" {
   name = "single-instance"
 
   ami                    = "ami-ebd02392"
-  instance_type          = "c5.4xlarge"
+  instance_type          = "c5.8xlarge"
   key_name               = "user1"
   monitoring             = true
   vpc_security_group_ids = ["sg-12345678"]
